9f99e2f40d66d87b37e2bfb29f191a05e32584bd
Natalia Luiza <natalialuizas@live.com>
2020-07-20 21:03:14 -0300

    ajustando animação do main

--- src/pages/Home/Main/index.js
+++ src/pages/Home/Main/index.js
@@ -3,25 +3,24 @@ import { HashLink as Link } from 'react-router-hash-link';
 import "./style.css";
 
 import personas from '../../../assets/images/persons__group.png'
-import personasMobile from '../../../assets/images/persons__group__mobile.png'
 import personWomen from '../../../assets/images/person_sobre.png'
 const Main = () => {
   return (
     <section className="main">
-      <div className="main__infos">
-        <div>
+      <div className="main__container">
+        <div className="main__infos">
         <h1 className="main__title">Procurando <br /> bootcamp?</h1>
       <p className="main__paragraph">
-        Aqui você encontra informações dos bootcamps gratuitos em único lugar.
+        Aqui você encontra informações dos<br /> bootcamps gratuitos em único lugar.
       </p>
       <Link to="/#bootcamps" className="btn__bootcamp">Buscar Bootcamp </Link>
       </div>
+      <img className="about__person__women" src={personWomen} alt="grupo de pessoas"></img>
       </div>
       <div className="main__img">
       <img className="img__desktop" src={personas} alt="grupo de pessoas"></img>
-      <img className="img__mobile" src={personasMobile} alt="grupo de pessoas"></img>
       </div>
-      {/* <img className="img__women" src={personWomen}></img> */}
+     
     </section>
   );
 };
