249d9d292bbb6b192227e0a86b9576bc9c19ceea
Natalia Luiza <natalialuizas@live.com>
2020-07-23 09:59:17 -0300

    add target no title dos cards

--- src/components/Title/Index.js
+++ src/components/Title/Index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './style.css'
 
-const MainTitle = ({ text, className, link }) => {
-    return <h1 className={className}><a href={link}>{text}</a></h1> 
+const Title = ({ text, className, link }) => {
+    return <h1 className={className}><a href={link} target="_blank">{text}</a></h1> 
 }
 
-export default MainTitle
+export default Title
